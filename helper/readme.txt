4c04757d40eaf33537988b6338b4fefe3ca09e96
AnnevanGils <amhvgils@outlook.com>
2018-09-28 17:57:15 +0200

    added project

---
+++ helper/readme.txt
@@ -0,0 +1,7 @@
+The helper program (requires Java 8).
+
+To check an input/output pair for validity and compute the score: 
+1. open a command-line window
+2. run the command (replace the two txt file names with the input/output pair you want to check):
+	java -jar VSGridMaps.jar -check example.txt example_output.txt
+3. Read off the results...
---
+++ src/vsgridmaps/Coordinate.java
@@ -0,0 +1,24 @@
+package vsgridmaps;
+
+public class Coordinate {
+	private double x,y;
+	
+	public Coordinate(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void set(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+
+}
